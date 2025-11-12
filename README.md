b2ee930a42165031c7e54613fc6ef989de079a9e
Alex Power <alexpower9@gmail.com>
2025-11-12 16:24:18 -0500

    Init setup

---
+++ README.md
@@ -0,0 +1,4 @@
+# CSC-375-final
+
+If you are running this for the first time, ensure you create a virtual environment, activate it
+and install from requirements.txt. You will also need pytorch installed on your machine
---
+++ example.py
@@ -0,0 +1,44 @@
+# just an example file of providing a prompt and having the qwen3 return the answer to us
+# this is just the code from the huggingface website
+from transformers import AutoModelForCausalLM, AutoTokenizer
+
+model_name = 'Qwen/Qwen3-0.6B'
+
+tokenizer = AutoTokenizer.from_pretrained(model_name)
+model = AutoModelForCausalLM.from_pretrained(
+    model_name,
+    torch_dtype="auto",
+    device_map="auto"
+)
+
+prompt = "Give me a short introduction of what you are!"
+messages = [
+    {"role": "user", "content": prompt}
+]
+text = tokenizer.apply_chat_template(
+    messages,
+    tokenize=False,
+    add_generation_prompt=True,
+    enable_thinking=True # Switches between thinking and non-thinking modes. Default is True.
+)
+model_inputs = tokenizer([text], return_tensors="pt").to(model.device)
+
+generated_ids = model.generate(
+    **model_inputs,
+    max_new_tokens=32768
+)
+output_ids = generated_ids[0][len(model_inputs.input_ids[0]):].tolist() 
+
+# parsing thinking content
+try:
+    # rindex finding 151668 (</think>)
+    index = len(output_ids) - output_ids[::-1].index(151668)
+except ValueError:
+    index = 0
+
+thinking_content = tokenizer.decode(output_ids[:index], skip_special_tokens=True).strip("\n")
+content = tokenizer.decode(output_ids[index:], skip_special_tokens=True).strip("\n")
+
+# uncomment the line below if you want to see the thinking context
+# print("thinking content:", thinking_content)
+print("content:", content)
